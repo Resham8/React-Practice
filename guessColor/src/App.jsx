ad633c3e1b9877894509443dc20bbefc5fa06190
Resham <reshamupadhyay48@gmail.com>
2025-03-11 09:28:49 +0530

    added score counter and final polishe

--- guessColor/src/App.jsx
+++ guessColor/src/App.jsx
@@ -2,38 +2,44 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import ColorCard from "./colorCard";
 
-function App() {  
-
+function App() {
   const [options, setOptions] = useState([]);
   const [correctAns, setCorrectAns] = useState(null);
 
   useEffect(() => {
-    refreshGame()
-  }, [])
+    refreshGame();
+  }, []);
 
-  function refreshGame(){
+  function refreshGame() {
     const newCorrectAns = generateRandomRGB();
     setCorrectAns(newCorrectAns);
 
-    const randomOptions = [generateRandomRGB(),generateRandomRGB(),generateRandomRGB()];
+    const randomOptions = [
+      generateRandomRGB(),
+      generateRandomRGB(),
+      generateRandomRGB(),
+    ];
     randomOptions.push(newCorrectAns);
-    setOptions(randomOptions.sort(() => Math.random() - 0.5))
+    setOptions(randomOptions.sort(() => Math.random() - 0.5));
   }
 
   return (
     <>
-      <div className="container">
-        <ColorCard randomRGB={correctAns} options={options} correctAns={correctAns} refreshGame={refreshGame} ></ColorCard>
-      </div>
-    </> 
+      <ColorCard
+        randomRGB={correctAns}
+        options={options}
+        correctAns={correctAns}
+        refreshGame={refreshGame}
+      ></ColorCard>
+    </>
   );
 }
 
-function generateRandomNo(){
+function generateRandomNo() {
   return Math.floor(Math.random() * 256);
 }
 
-function generateRandomRGB(){
+function generateRandomRGB() {
   const red = generateRandomNo();
   const green = generateRandomNo();
   const blue = generateRandomNo();
--- guessColor/src/colorCard.jsx
+++ guessColor/src/colorCard.jsx
@@ -1,45 +1,57 @@
 import React, { useEffect, useState } from "react";
 
-
-const ColorCard = ({randomRGB, options, correctAns, refreshGame}) => {
+const ColorCard = ({ randomRGB, options, correctAns, refreshGame }) => {
   const [guessColor, setguessColor] = useState(null);
   const [message, setMessage] = useState(null);
+  const [score, setScore] = useState(0);
 
-  useEffect(()=>{
-    setguessColor(randomRGB)
-  }, [randomRGB])
+  useEffect(() => {
+    setguessColor(randomRGB);
+  }, [randomRGB]);
 
-  function winDecide(color){
-    if(color == correctAns){
-      setMessage("🎉 Yay correct ans! 🥳")
+  function winDecide(color) {
+    if (color == correctAns) {
+      setMessage("🎉 Yay correct ans! 🥳");
+      setScore(score + 1);
       setTimeout(() => {
-        setMessage(""); 
+        setMessage("");
         refreshGame();
       }, 1000);
     } else {
-      setMessage("Try another guess 🤨")
+      setMessage("Try another guess 🤨");
     }
+  }
+
+  function newGame() {
+    refreshGame();
+    setScore(0);
+    setMessage("");
   }
 
   return (
     <>
-      <h1>Guess The Color</h1>
-      <p className="color-code">{guessColor}</p>
-      <p>Pick the correct color:</p>
-      <div className="options">
-        {options.map((color, index) => (          
-          <button
-          key={index}
-          className="option-btn"
-          style={{ backgroundColor: `${color}` }}
-          onClick={() => winDecide(color)}
-        ></button>))}      
+      <div className="container">
+        <span>🏆 {score}</span>
+        <h1>Guess The Color</h1>
+        <p className="color-code">{guessColor}</p>
+        <p>Pick the correct color:</p>
+        <div className="options">
+          {options.map((color, index) => (
+            <button
+              key={index}
+              className="option-btn"
+              style={{ backgroundColor: `${color}` }}
+              onClick={() => winDecide(color)}
+            ></button>
+          ))}
+        </div>
+        <p className="message">{message}</p>
       </div>
-      <p className="message">{message}</p>
+      <button className="btn-new-game" onClick={newGame}>
+        New Game
+      </button>
     </>
   );
 };
-
-
 
 export default ColorCard;
