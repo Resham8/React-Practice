4930699443e2fdcd42770b04f7c807d549979715
Resham <reshamupadhyay48@gmail.com>
2025-03-14 00:42:52 +0530

    created submit function

--- petAdoption/src/components/PetAdoptionForm.jsx
+++ petAdoption/src/components/PetAdoptionForm.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+
 const PetAdoptionForm = () => {
+
+  const [values, setValues] = useState({
+    petName: '',
+    petTypes: '',
+    bread: '',
+    yourName: '',
+    email:'',
+    phone: ''
+  })
+
+  function handleChanges(e){
+    setValues({...values, [e.target.name]:[e.target.value]})
+  }
+
+  function handleSubmit(event) {   
+    event.preventDefault(); 
+    console.log(values)
+  }
+
   return (
-    <div className="form">
+    <>
+    <form className="form" onSubmit={handleSubmit}>
       <div>
         <label>Pet Name</label>
-        <input type="text" />
+        <input type="text" name="petName" onChange={(e) => handleChanges(e)} />
       </div>
       <div>
         <label>Pet Type</label>
-        <select>
+        <select name="petTypes" onChange={(e) => handleChanges(e)}>
           <option value="dog">Dog</option>
           <option value="cat">Cat</option>
           <option value="bird">Bird</option>
@@ -21,24 +43,26 @@ const PetAdoptionForm = () => {
       </div>
       <div>
         <label>Bread</label>
-        <input type="text" />
+        <input type="text" name="bread" onChange={(e) => handleChanges(e)} />
       </div>
       <div>
         <label>Your Name</label>
-        <input type="text" />
+        <input type="text" name="yourName" onChange={(e) => handleChanges(e)} />
       </div>
       <div>
         <label>Email</label>
-        <input type="email" />
+        <input type="email" name="email" onChange={(e) => handleChanges(e)} />
       </div>
       <div>
         <label>Phone</label>
-        <input type="text" />
+        <input type="text" name="phone" onChange={(e) => handleChanges(e)} />
       </div>
-      <div>
-        <button>Submit</button>
+      <div style={{display:"flex", justifyContent:"space-between", gap:"10px"}}>
+        <button type="submit">Submit</button>
+        <button>Reset</button>
       </div>
-    </div>
+    </form>
+    </>    
   );
 };
 
