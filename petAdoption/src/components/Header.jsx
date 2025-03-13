5e578a1207859b6c4a5a28524e1fdd47de8e23d0
Resham <reshamupadhyay48@gmail.com>
2025-03-14 00:01:20 +0530

    pet adoption form

--- petAdoption/src/components/Header.jsx
+++ petAdoption/src/components/Header.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-
 const Header = () => {
   return (
-    <div>Header</div>
+    <div style={{background: "linear-gradient(315deg, rgba(164, 38, 16, 0.7) 0%, rgba(224, 120, 38, 0.7) 100%)"
+      , color:"white", padding:".5rem", fontSize:"2rem", fontWeight:"400"}}>Pet Adoption</div>
   )
 }
 
--- petAdoption/src/components/PetAdoptionForm.jsx
+++ petAdoption/src/components/PetAdoptionForm.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
 const PetAdoptionForm = () => {
   return (
-    <div>PetAdoptionForm</div>
-  )
-}
+    <div className="form">
+      <div>
+        <label>Pet Name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Pet Type</label>
+        <select>
+          <option value="dog">Dog</option>
+          <option value="cat">Cat</option>
+          <option value="bird">Bird</option>
+          <option value="fish">Fish</option>
+          <option value="rabbit">Rabbit</option>
+          <option value="hamster">Hamster</option>
+          <option value="turtle">Turtle</option>
+          <option value="reptile">Reptile</option>
+          <option value="otter">Otter</option>
+        </select>
+      </div>
+      <div>
+        <label>Bread</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Your Name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" />
+      </div>
+      <div>
+        <label>Phone</label>
+        <input type="text" />
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+};
 
-export default PetAdoptionForm
+export default PetAdoptionForm;
