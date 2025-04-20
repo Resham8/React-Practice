39be4df5a676a678257848d48f97583c3c377ab5
Resham <reshamupadhyay48@gmail.com>
2025-04-20 22:42:14 +0530

    amazonCart
    navbar

--- amazonCart/src/components/Navbar.jsx
+++ amazonCart/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import { ShoppingCart } from "lucide-react";
 function Navbar() {
+  const cartCount = 3; // Replace with dynamic count from state or context
+
   return (
     <header>
       <div className="header-container">
         <h1>amazon.in</h1>
         <div className="header-right">
-            <span>Hello, user</span>
-            
+          <span>Hello, user</span>
+          <div className="cart-icon-wrapper">
+            <ShoppingCart color="white" size={24} />
+            <div className="cart-badge">{cartCount}</div>
+          </div>
         </div>
       </div>
     </header>
