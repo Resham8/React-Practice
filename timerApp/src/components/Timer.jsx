9bdc3063271a23f7c0750d7e69aa61fd20bec1e5
Resham <reshamupadhyay48@gmail.com>
2025-04-24 22:06:19 +0530

    Update Timer.jsx

--- timerApp/src/components/Timer.jsx
+++ timerApp/src/components/Timer.jsx
@@ -3,41 +3,21 @@ import style from "./Timer.module.css";
 import { formatTime, calculateTime } from "../utils/auxiliaryFunctions";
 
 const Timer = () => {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-  const [editState, setEditState] = useState(null);  
-
-  useEffect(() => {
-    if (!isRunning || time === 0) return; 
-
-    const interval = setInterval(() => {
-      setTime((prevTime) => {
-        if (prevTime <= 1) {
-          clearInterval(interval); 
-          setIsRunning(false);
-          return 0;
-        }
-        return prevTime - 1; 
-      });
-    }, 1000);
+    const [isRunning, setIsRunning] = useState(false);
     
-    return () => clearInterval(interval);
-  }, [isRunning, time]);
-
-  const { hours, minutes, seconds } = formatTime(time);
 
   return (
     <div className={style.timerApp}>
       <div className={style.timerDisplay}>
         <div className={style.timerCircle}>
           <div className={style.timerTime}>
-            {hours}:{minutes}:{seconds}
+            
           </div>
         </div>
       </div>
       <div className={style.actionButtons}>
-        <button className={style.actionButton} onClick={() => setIsRunning(!isRunning)}>
-          {isRunning ? 'Pause' : 'Start'}
+        <button className={style.actionButton}>
+          Start
         </button>
         <button className={style.actionButton}>
           Stop
