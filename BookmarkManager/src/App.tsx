cf3fb90d7611a962f858ebdf595da36078c2b58b
Resham <reshamupadhyay48@gmail.com>
2025-05-10 23:09:13 +0530

    Update App.tsx

--- BookmarkManager/src/App.tsx
+++ BookmarkManager/src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
+import Download from './components/Download'
+import Features from './components/Features'
+import FnQ from './components/FnQ'
 import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
+import NewsLetter from './components/NewsLetter'
 
 function App() {
 
@@ -9,6 +13,10 @@ function App() {
     <div className='overflow-x-hidden'>
       <Navbar/>
       <HeroSection/>
+      <Features/>
+      <Download/>
+      <FnQ/>
+      <NewsLetter/>
       <Footer/>
     </div>
   )
