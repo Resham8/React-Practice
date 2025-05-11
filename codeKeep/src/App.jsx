67d469f05026fac620d605a8a0016de421bed9aa
Resham <reshamupadhyay48@gmail.com>
2025-05-11 23:40:34 +0530

    added a navbar

--- codeKeep/src/App.jsx
+++ codeKeep/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import MainContent from "./components/MainContent";
+import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <>
-      <div className="flex bg-[#1a1a1a] max-w-screen min-h-screen">
+    <div className="bg-[#1a1a1a] max-w-screen min-h-screen">
+      <NavBar /> 
+      <div className="flex">
         <Sidebar />
         <MainContent />
       </div>
-    </>
+    </div>
   );
 }
 
